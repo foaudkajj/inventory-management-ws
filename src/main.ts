b1d94e0ebfb8b07e9edead7f2aa30d883df22f99
mfuatnuroglu <mfuatnuroglu@gmail.com>
2023-03-11 19:58:38 +0300

    swagger configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import {NestFactory} from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
+  const config = new DocumentBuilder()
+    .setTitle('Inventory Management API')
+    .setDescription('The Inventory Management API description')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .addSecurityRequirements('bearer')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
