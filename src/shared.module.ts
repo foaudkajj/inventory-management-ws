067a4662f4bc9ca158faff7b3abc5b44658ac75f
ibrahim issa basha <94835822+PASHA011@users.noreply.github.com>
2023-03-20 10:14:05 +0300

    #15

--- src/shared.module.ts
+++ src/shared.module.ts
@@ -2,10 +2,6 @@ import { CustomerInfoService } from './endpoints/customer-info/customer-info.ser
 import { CustomerInfoRepository } from './endpoints/customer-info/customer-info.repository';
 import { CustomerInfoController } from './endpoints/customer-info/customer-info.controller';
 import { CustomerInfo } from 'src/models';
-import { PaymentMethodService } from './endpoints/payment-method/payment-method.service';
-import { PaymentMethodRepository } from './endpoints/payment-method/payment-method.repository';
-import { PaymentMethodController } from './endpoints/payment-method/payment-method.controller';
-import { PaymentMethod } from './models/payment-method.model';
 import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {ColorController} from './endpoints/color/color.controller';
@@ -17,10 +13,10 @@ import { CurrencyService } from './endpoints/currency/Currency.service';
 import {Color, Currency} from './models';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Color,Currency,PaymentMethod,CustomerInfo])],
-  controllers: [ColorController,CurrencyController,PaymentMethodController,CustomerInfoController],
-  providers: [ColorService, ColorRepository,CurrencyRepository,CurrencyService,PaymentMethodRepository,PaymentMethodService,CustomerInfoRepository,CustomerInfoService],
-  exports: [ColorService, ColorRepository,CurrencyRepository,CurrencyService,PaymentMethodRepository,PaymentMethodService,CustomerInfoRepository,CustomerInfoService],
+  imports: [TypeOrmModule.forFeature([Color,Currency,CustomerInfo])],
+  controllers: [ColorController,CurrencyController,CustomerInfoController],
+  providers: [ColorService, ColorRepository,CurrencyRepository,CurrencyService,CustomerInfoRepository,CustomerInfoService],
+  exports: [ColorService, ColorRepository,CurrencyRepository,CurrencyService,CustomerInfoRepository,CustomerInfoService],
 })
 /**
  * This module contains only system wide used services (like repositories)
