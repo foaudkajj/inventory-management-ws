1d413d664d0d190a78294cfa9e2c6152f96b82df
mfuatnuroglu <mfuatnuroglu@gmail.com>
2023-07-23 09:53:57 +0300

    handle not existing user

--- src/endpoints/auth/auth.service.ts
+++ src/endpoints/auth/auth.service.ts
@@ -1,28 +1,40 @@
-import { UserService } from "../user/user.service";
-import { JwtService } from '@nestjs/jwt';
-import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
-import { LoginRequest } from "src/models";
-import { compare } from "bcryptjs";
+import {UserService} from '../user/user.service';
+import {JwtService} from '@nestjs/jwt';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import {LoginRequest, User} from 'src/models';
+import {compare} from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService, private jwtService: JwtService) { }
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-    async signIn(userReq: LoginRequest): Promise<any> {
-
-        const user = await this.userService.findOne(userReq.username);
-        const hash = await compare(userReq.password, user.password);
-        if (!hash) {
-            throw new HttpException(
-                'ERROR.WRONG_USERNAME_PASSWORD',
-                HttpStatus.UNAUTHORIZED,
-            );
-        }
-
-        const payload = { sub: user.id, username: user.username, merchantId: user.merchantId, branchId: user.branchId };
-        return {
-            access_token: await this.jwtService.signAsync(payload),
-            user
-        };
+  async signIn(
+    userReq: LoginRequest,
+  ): Promise<{access_token: string; user: User}> {
+    const user = await this.userService.findOne(userReq.username);
+    if (!user) {
+      throw new HttpException('ERROR.WRONG_USERNAME', HttpStatus.UNAUTHORIZED);
     }
+    const hash = await compare(userReq.password, user.password);
+    if (!hash) {
+      throw new HttpException(
+        'ERROR.WRONG_USERNAME_PASSWORD',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      merchantId: user.merchantId,
+      branchId: user.branchId,
+    };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user,
+    };
+  }
 }
