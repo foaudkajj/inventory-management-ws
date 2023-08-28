078e04eebff4500524db3a8f2e036ee0d9f3d65f
ibrahim issa basha <94835822+PASHA011@users.noreply.github.com>
2023-08-28 12:47:38 +0300

    #61

--- src/endpoints/customer-info/customer-info.service.ts
+++ src/endpoints/customer-info/customer-info.service.ts
@@ -1,23 +1,28 @@
-import {Injectable} from '@nestjs/common';
-import {CustomerInfo} from 'src/models';
-import {CustomerInfoRepository} from './customer-info.repository';
+import { Injectable } from '@nestjs/common';
+import { CustomerInfo } from 'src/models';
+import { CustomerInfoRepository } from './customer-info.repository';
+import { ClsService } from 'nestjs-cls';
 
 @Injectable()
 export class CustomerInfoService {
-  constructor(private customerInfoRepository: CustomerInfoRepository) {}
+  constructor(private customerInfoRepository: CustomerInfoRepository,
+    private readonly cls: ClsService,
+  ) { }
   getAll(): Promise<CustomerInfo[]> {
     return this.customerInfoRepository.orm.find();
   }
 
   insert(row: CustomerInfo) {
+    row.merchantId = this.cls.get('user.merchantId');
     return this.customerInfoRepository.orm.insert(row);
   }
 
   update(row: Partial<CustomerInfo>, id: string) {
-    return this.customerInfoRepository.orm.update({id: id}, row);
+    row.merchantId = this.cls.get('user.merchantId');
+    return this.customerInfoRepository.orm.update({ id: id }, row);
   }
 
   delete(id: string) {
-    return this.customerInfoRepository.orm.delete({id: id});
+    return this.customerInfoRepository.orm.delete({ id: id });
   }
 }
